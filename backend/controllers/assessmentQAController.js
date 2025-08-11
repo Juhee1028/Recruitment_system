c9fc346d5a0ff7c34ddc821222ceb815aabda187
Juhee, Park <miline1028@gmail.com>
2025-08-11 17:19:49 +1000

    modified syntax errors in server.js file and assessmentQAController.js file

--- backend/controllers/assessmentQAController.js
+++ backend/controllers/assessmentQAController.js
@@ -1,10 +1,10 @@
 const AssessmentQA = require('../models/AssessmentQA');
-const JobPost = require('../models/AssessmentQA');
+const JobPost = require('../models/JobPost');
 
 
 const getQuestions = async (req, res) => {
     try {
-        const questions = await AssessmentQA.find({jobId: req.job.id});
+        const questions = await AssessmentQA.find({jobId: req.params.jobId});
         res.json(questions);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -13,10 +13,13 @@ const getQuestions = async (req, res) => {
 
 
 const addQuestion = async (req, res) => {
-    const { questionText, correctAnswer, timeLimit } = req.body;
+    const { jobId, questionText, correctAnswer, timeLimit } = req.body;
+    if (!jobId){
+        return res.status(400).json({ message: 'jobId is required'});
+    }
     try {
-        const newQuestion = await JobPost.create({
-            jobId: req.job.id,
+        const newQuestion = await AssessmentQA.create({
+            jobId,
             questionText,
             correctAnswer,
             timeLimit,
@@ -33,9 +36,9 @@ const updateQuestion = async (req, res) => {
         const question = await AssessmentQA.findById (req.params.id);
         if (!question) return res.status(404).json({ message: 'question not found'});
 
-        job.questionText = questionText || question.questionText;
-        job.correctAnswer = correctAnswer || question.correctAnswer;
-        job.timeLimit = timeLimit ?? question.timeLimit;
+        question.questionText = questionText || question.questionText;
+        question.correctAnswer = correctAnswer || question.correctAnswer;
+        question.timeLimit = timeLimit ?? question.timeLimit;
 
         const updatedQuestion = await question.save();
         res.json(updatedQuestion);
